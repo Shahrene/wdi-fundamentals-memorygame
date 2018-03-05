6fe658866bf2568b2c22ba25634436ef488aeb3b
Shahrene Davey <shahrene@Shahrenes-MacBook-Pro.local>
2018-03-05 21:56:16 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+      rank: "king",
+      suit: "hearts",
+      cardImage: "images/king-of-hearts.png"
+    },
+    {
+      rank: "king",
+      suit: "diamonds",
+      cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
-var checkForMatch = function(); {
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]);
     console.log("You found a match");
-  } else {
+  } else if {
     console.log("Sorry, try again");
   }
-var flipCard = function(cardID); {
+var flipCard = function(cardID) {
   flipCard(0);
   flipCard(2);
   checkForMatch();
-console.log("User flipped "+ cards[cardID]);
-cardsInPlay.push(cards[cardID]);
+console.log("User flipped " + cards[cardID].rank);
+cardsInPlay.push(cards[cardID].rank);
+console.log(cardImage);
+console.log(suit);
 }
